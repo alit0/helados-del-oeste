bfd2236e17ec41ffd20639c93a86c5a71dfeb8c7
alit0 <romero.sbn@gmail.com>
2026-07-08 13:06:21 -0300

    fix: point catalog URL to the v3 Apps Script deployment (adds pesoPrices)

--- src/data/catalogUrl.ts
+++ src/data/catalogUrl.ts
@@ -7,7 +7,7 @@
 // bundled local snapshot for GET and disable the network POST (used in tests).
 
 const DEFAULT_CATALOG_URL =
-  'https://script.google.com/macros/s/AKfycbzlyyT22fhRRwikw30gVigxoW7mnpe2iKSZAOhIxz_iHzluRQqweyUd7GuIfIbCaGEiLg/exec';
+  'https://script.google.com/macros/s/AKfycbyqrB0eHg7fURb_O4HoXBfL-tyKLEfhnQSjS-NgreRgf9eT61df-hoyBuO-I3j50gJ9eA/exec';
 
 export const CATALOG_URL =
   import.meta.env.VITE_CATALOG_URL !== undefined
